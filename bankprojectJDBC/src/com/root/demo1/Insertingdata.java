d81c470391b0bf9be8d4f38611a8f31f68c3a4d2
niketanthombare <niketanthombare45@gmail.com>
2021-01-28 03:31:29 +0530

    changes done inserting data..

--- bankprojectJDBC/src/com/root/demo1/Insertingdata.java
+++ bankprojectJDBC/src/com/root/demo1/Insertingdata.java
@@ -10,6 +10,7 @@ public class Insertingdata {
 
 	public static void main(String[] args) 
 	{
+		//query added...
 		try 
 		{
 			Class.forName("com.mysql.jdbc.Driver");
@@ -18,7 +19,7 @@ public class Insertingdata {
 			String query2="insert into student values(6,'Amol','Amol@gmail.com','Amol123')";
 			String query3="insert into student values(7,'Ramesh','Ramesh@gmail.com','Ramesh123')";
 			Statement stmt=con.createStatement();
-			ResultSet rs=stmt.executeQuery(query);
+			ResultSet rs=stmt.executeQuery(query1);
 			
 		}
 		catch (ClassNotFoundException |SQLException e) 
